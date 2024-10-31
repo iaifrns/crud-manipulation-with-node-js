83df952ba6f94d2af59e34885655b46aae3a5289
iaifrns <nsinifranc@gmail.com>
2024-10-31 14:02:45 +0100

    finished with the create of role

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
 
-const __filename = fileURLToPath(import.meta.url)
-const __direname = path.dirname(__filename)
-const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __direname = path.dirname(__filename);
+const app = express();
 
-const __publicfolder = path.join(__direname, 'public')
+const __publicfolder = path.join(__direname, "public");
 
-app.use(express.static(__publicfolder))
+app.use(express.static(__publicfolder));
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__publicfolder, 'mainPage.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__publicfolder, "mainPage.html"));
+});
 
-app.get('/admin', (req, res) => {
-    res.sendFile(path.join(__publicfolder, 'createUserPage.html'))
-})
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__publicfolder, "createUserPage.html"));
+});
 
-const port = '3000'
+app.post("/create_role", (req, res) => {
+  const data = req.body;
+  fs.writeFile(
+    path.join(__publicfolder, 'json', "role.json"),
+    JSON.stringify(data, null, 2),
+    (err) => {
+      if (err) {
+        res.json({ error: true, message: "an error occured" });
+      }
+      res.json({ message: "Role Created" });
+    }
+  );
+});
+
+const port = "3000";
 
 app.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}`)
-})
-
+  console.log(`Server running on port http://localhost:${port}`);
+});
--- public/js/adminData.js
+++ public/js/adminData.js
@@ -1,19 +1,23 @@
 const menuContainer = document.getElementById("menu-container");
 const roleContainer = document.getElementById("role-container");
+const mainContainer = document.querySelector(".main-container");
+const loader = document.getElementById("loader");
 
-const showRoleSection = () => {
+let roleList;
+
+const showRoleSection = async () => {
   erraseAllSections();
+  mainContainer.classList.add("erraser");
+  loader.style.display = "flex";
+  await createRoleFile("../json/role.json");
+  loader.style.display = "none";
+  mainContainer.classList.remove("erraser");
   roleContainer.style.display = "flex";
 };
 
-const createRole = () => {
-  let roleList;
-  createRoleFile("../json/role.json");
-  /* if (checkFileExist('../json/role.json')){
-        console.log('file exist')
-    }else {
-        console.log('file does not exist')
-     }*/
+const createRole = async () => {
+  await createRoleFile("../json/role.json");
+  console.log("here you are");
 };
 
 const erraseAllSections = () => {
@@ -21,8 +25,28 @@ const erraseAllSections = () => {
   roleContainer.style.display = "none";
 };
 
-const createRoleFile = new Promise((res) => {
-  fetch(res.apply)
-    .then((response) => console.log(response))
-    .catch((err) => console.log(err));
-});
+const createRoleFile = async (file) => {
+  try {
+    const response = await fetch(file);
+    if (response.status == 404) {
+      console.log("here ...");
+      await fetch("/create_role", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          roleList = Object.values({});
+          console.log(data.message,roleList);
+        })
+        .catch((error) => console.log(error));
+    }else {
+      console.log(response)
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
